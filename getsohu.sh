88c2f58d767c9b7644a8158e79de6f1316fe9959
Wu Yongzheng <wuyongzheng@gmail.com>
2010-10-12 17:36:06 +0800

    add doc as comment

--- getsohu.sh
+++ getsohu.sh
@@ -28,9 +28,17 @@ if ! wget -q -O /tmp/getsohu2.txt "http://hot.vrs.sohu.com/vrs_flash.action?vid=
 	echo wget "http://hot.vrs.sohu.com/vrs_flash.action?vid=$vid" failed.
 	exit
 fi
+# It should be a json file. The array pointed by "clipsURL" is what I want.
+# Example:
+# {"prot":2,"allot":"220.181.61.229","tn":5,"sp":1024,"status":1,"play":1,"pL":30,
+# "url":"http://tv.sohu.com/20090701/n264901824.shtml","uS":-1,"fms":0,
+# "data":{"tvName":"刀锋1937第5集","ch":"tv","fps":25,"ipLimit":0,"width":0,
+# "clipsURL":["http://data.vod.itc.cn/tv/20090701/ea8269a6-1c3e-4a71-af49-50f4bf351ead.mp4",
+# "http://data.vod.itc.cn/tv/20090701/46c03c38-7cc7-40d6-a0bb-78c0bb93236b.mp4",
+# ...
 
 mp4list=`binreplace -r '"' '\n' /tmp/getsohu2.txt | grep '^http://.*mp4$'`
-# it should be a list of mp4 urls. example:
+# It should be a list of mp4 urls. Example:
 # http://data.vod.itc.cn/tv/20100505/d2f89e5b-e6a4-4ddf-a765-e006028926c0.mp4
 
 if [ "x$mp4list" = x ] ; then
